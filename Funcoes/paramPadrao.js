b4b6f92fc856bf2589b07ee3295b5037e2cdc7dd
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-08 21:47:42 -0300

    gerando valor padrao 1

--- Funcoes/paramPadrao.js
+++ Funcoes/paramPadrao.js
@@ -6,4 +6,23 @@ function soma1() {
   return a + b + c;
 }
 
-console.log(soma(), soma(3), soma(1, 2, 3), soma(1, 2, 3, 4));
+console.log(soma(), soma(3), soma(1, 2, 3), soma(0, 0, 0));
+
+//sempre procurar por valor padrao em parametro de funcao
+
+function soma2() {
+  a = a !== undefined ? a : 1;
+  b = 1 in arguments ? b : 1;
+  c = isNaN(c) ? 1 : c;
+  return a + b + c;
+}
+
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(1, 2, 3));
+
+//metodo ec2015
+
+function soma(a = 1, b = 1, c = 1) {
+  return a + b + c;
+}
+
+console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(1, 2, 3));
