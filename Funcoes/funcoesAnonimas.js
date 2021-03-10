d4d89763eb8bbee28e46a10bd51703e959475614
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-10 12:00:16 -0300

    arrow function  como parametro de outra funcao

--- Funcoes/funcoesAnonimas.js
+++ Funcoes/funcoesAnonimas.js
@@ -5,3 +5,9 @@ const soma = function (x, y) {
 const imprimiResultado = function (a, b, operacao = soma) {
   console.log(operacao(3, 4));
 };
+
+imprimiResultado(3, 4, function (x, y) {
+  return x - y;
+});
+
+imprimiResultado(3, 4, (x, y) => x * y);
