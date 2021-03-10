b6a9f8b1e88bbf1623f98f9005d49eabe8c66f38
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-10 12:06:06 -0300

    funcao anonima dentro de objeto novo jeito

--- Funcoes/funcoesAnonimas.js
+++ Funcoes/funcoesAnonimas.js
@@ -13,3 +13,18 @@ imprimiResultado(3, 4, function (x, y) {
 });
 
 imprimiResultado(3, 4, (x, y) => x * y);
+
+const pessoa = {
+  falar: function () {
+    console.log("Opa");
+  },
+};
+
+pessoa.falar();
+
+//nova forma de declarar funcoes dentor de objeto
+const pessoa = {
+  falar() {
+    console.log("Opa");
+  },
+};
