21b75133b5989c78cf91206c2ebec0d7d43760c0
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-11 15:10:59 -0300

    Exemplo factory com parametro

--- Funcoes/funcaoFactory.js
+++ Funcoes/funcaoFactory.js
@@ -7,3 +7,16 @@ function criarPessoa() {
 }
 
 console.log(criarPessoa());
+
+//factory passando parametros
+
+function criarProduto(nome, preco) {
+    return {
+        nome = this.nome,
+        preco,
+        desconto: 0.1
+    }
+}
+ 
+
+console.log(criarProduto('Notbook', 2.199))
