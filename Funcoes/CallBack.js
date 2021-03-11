cf800c7d7d76d4256b2551676b3e9af9f02b7dca
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-11 09:41:29 -0300

    Exemplo sem call Back

--- Funcoes/CallBack.js
+++ Funcoes/CallBack.js
@@ -9,3 +9,16 @@ fabricantes.forEach(imprimir);
 fabricantes.forEach((fabricante) => {
   console.log(fabricante);
 });
+
+//exemplo sem callback
+
+const notas = [7.1, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0];
+
+const notasBaixas1 = [];
+
+for (let i in notas) {
+  if (notas < 7) {
+    notasBaixas1.push(notas[i]);
+  }
+}
+console.log(notasBaixas1);
