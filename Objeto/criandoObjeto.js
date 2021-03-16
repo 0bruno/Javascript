3cae25515bbe3aafa86d9935c0d12d80f69f426c
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-16 15:19:34 -0300

    feat: create object from function constructors

--- Objeto/criandoObjeto.js
+++ Objeto/criandoObjeto.js
@@ -5,3 +5,14 @@ const obj1 = {};
 console.log(typeof Object, typeof new Object());
 const obj2 = new Object();
 console.log(obj2);
+
+//Funções Construtoras
+function Produto(nome, preco, desc) {
+  this.nome = nome;
+  this.getPrecoDesconto = () => {
+    return preco + (1 - desc);
+  };
+}
+
+const p1 = new Produto("Caneta", 7.99, 0.15);
+const p2 = new Produto("Celular, 1850", 0.1);
