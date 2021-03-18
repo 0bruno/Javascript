a15968231fa3ae30c17f093567f0a404ab495e26
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-18 16:38:24 -0300

    feat: example slice()

--- Array/araay.js
+++ Array/araay.js
@@ -9,7 +9,11 @@ console.log(pilotos);
 
 pilotos.unshift("Hamilton"); //adiciona como primeiro elemento
 
-//splice
+//splice pegar pedaço do array
 pilotos.splice(2, 0, "Botas", "Massa"); // adicionando a aprtir do indice dois
 
 pilotos.splice(2, 1); // a partir do indice 2 demover um
+
+const pilotos2 = pilotos.slice(2); // a partir do 2 indice retorna um novo array para pilotos 2
+
+const pilotos3 = pilotos.slice(1, 4); //do 1 até o 4 retorna um novo array até o indice 4, este por sinal nao entra
