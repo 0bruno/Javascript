c3866d9d5b4b010a07ce90c9a259a7c3e7480f4a
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-18 16:33:01 -0300

    feat: example shift() and unshift()

--- Array/araay.js
+++ Array/araay.js
@@ -3,3 +3,8 @@ pilotos.pop(); //removendo ultima posição
 
 pilotos.push("Verstappen"); // Adicionando após o ultimo elemento do array
 console.log(pilotos);
+
+pilotos.shift(); //remove o primeiro
+console.log(pilotos);
+
+pilotos.unshift("Hamilton"); //adiciona como primeiro elemento
