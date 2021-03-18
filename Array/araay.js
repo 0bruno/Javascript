fd321abf1463ae84c37840f96037904df683ffb3
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-18 16:31:37 -0300

    feat: example push()

--- Array/araay.js
+++ Array/araay.js
@@ -1,2 +1,5 @@
 const pilotos = ["Vettel", "Alonso", "Raikkonen", "Massa"];
 pilotos.pop(); //removendo ultima posição
+
+pilotos.push("Verstappen"); // Adicionando após o ultimo elemento do array
+console.log(pilotos);
