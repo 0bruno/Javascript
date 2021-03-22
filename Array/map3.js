8ff0288db1b543c4d20bc1b845e4b02550ab0a9c
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-22 16:37:28 -0300

    feat: customize map function

---
+++ Array/map3.js
@@ -0,0 +1,24 @@
+Array.prototype.map2 = function (callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this));
+  }
+  return newArray;
+};
+
+const carrinho = [
+  '{"nome": "Borracha", "preco": 3.45}',
+  '{"nome": "Caderno", "preco": 13.90}',
+  '{"nome": "kit de lapis", "preco": 41.22}',
+  '{"nome": "Caneta", "preco": 7.50}',
+];
+
+const paraObjeto = (json) => {
+  return JSON.parse(json);
+};
+
+const apenasPreco = (produto) => {
+  return produto.preco;
+};
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco);
