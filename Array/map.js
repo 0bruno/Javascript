e3f3ffdb32bd61ce1c2394e1cd4b81b4fc0f9994
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-22 15:24:04 -0300

    feat: map Example

--- Array/map.js
+++ Array/map.js
@@ -4,3 +4,13 @@ const nums = [1, 2, 3, 4, 5];
 let resultado = nums.map(function (e) {
   return e * 2;
 });
+
+const soma10 = (e) => {
+  return e + 10;
+};
+
+const triplo = (e) => e * 3;
+
+const paraDinheiro = (e) => `R$${parseFloat(e).toFixed(2).replace(".", ",")}`;
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro);
