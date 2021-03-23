09ed28e834233ae8699f77382dfa069807110ce8
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-23 15:07:37 -0300

    feat: example function concat()

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ["Rubia", "Cintia"];
+const filhos = ["Bruno", "Bruno II"];
+
+const todos = filhos.concat(filhas);
+console.log(todos, filhas, filhos);
+
+console.log(["a", "b"].concat([1, 2], [3, 4], [[5, 6]]));
