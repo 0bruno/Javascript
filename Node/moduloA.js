f54c287519c9269dbf79c4534b0cef8777e16eb7
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-24 15:37:33 -0300

    feat: export ES6

--- Node/moduloA.js
+++ Node/moduloA.js
@@ -1,3 +1,8 @@
 this.ola = "fala Pessoal";
 exports.bemVindo = "Bem vindo ao node";
 module.exports.ateLogo = "Até o proximo evento";
+
+//ES6
+this.ola = "fala Pessoal";
+export const bemVindo = "Bem vindo ao node";
+export const ateLogo = "Até o proximo evento";
