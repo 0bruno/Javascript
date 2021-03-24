2623e0966128947cd39eb98ca85ff3d29c2e382b
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-23 23:40:35 -0300

    refactor: add line comentary

--- Array/map3.js
+++ Array/map3.js
@@ -12,7 +12,7 @@ const carrinho = [
   '{"nome": "kit de lapis", "preco": 41.22}',
   '{"nome": "Caneta", "preco": 7.50}',
 ];
-
+//criando funcao
 const paraObjeto = (json) => {
   return JSON.parse(json);
 };
