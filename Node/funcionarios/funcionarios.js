f55107cfd6a96e665fa37d8d9d5a44b2647180a4
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-25 10:27:19 -0300

    feat: example receive json returned woman chinese witch lower salary

--- Node/funcionarios/funcionarios.js
+++ Node/funcionarios/funcionarios.js
@@ -5,18 +5,17 @@ const axios = require("axios");
 
 //obter informacoes do servidor com promisse
 //obter a mulher chinesa com menor salario
-const chineses = (funcionario) => funcionario.pais === "China";
-const mulheres = (funcionario) => funcionario.genero === "F";
+const chinesas = (funcionarios) => funcionarios.pais === "China";
+const mulheres = (funcionarios) => funcionarios.genero === "F";
 const menorSalario = (func, funcAtual) => {
-  return func.salario < funcAtual ? func : funcAtual;
+  return func.salario < funcAtual.salario ? func : funcAtual;
 };
 
 axios.get(url).then((response) => {
   const funcionarios = response.data;
   const func = funcionarios
-    .filter(chineses)
+    .filter(chinesas)
     .filter(mulheres)
     .reduce(menorSalario);
-
   console.log(func);
 });
