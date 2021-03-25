695f169dc2f7e98732612ad2855c2a3829b2c555
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-25 16:37:42 -0300

    refactor: add commentary line

--- Node/passandoParametroCliente.js
+++ Node/passandoParametroCliente.js
@@ -1,2 +1,3 @@
+//require e uma funcao, posso chamar ja passando parametros pro modulo que esta a funcao feita
 const saudacao = require("./passandoParametros")("Bruno", "Cinthia", "Rúbia");
 console.log(saudacao);
