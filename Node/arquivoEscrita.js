745cb75196a51ab0e7400636ecb52f7e7b6adf67
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-27 11:03:04 -0300

    feat: read file on require to extension.json

--- Node/arquivoEscrita.js
+++ Node/arquivoEscrita.js
@@ -1,13 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
 
 //persistindo e salvando em disco com writeFile()
 const produto = {
-    nome: 'Celular',
-    preco: 1249.99,
-    desconto: 0.15
-}
+  nome: 'Celular',
+  preco: 1249.99,
+  desconto: 0.15,
+};
 
 //gerando aruivo.json e salvando em disco com o writeFile()
-fs.writeFile(__dirname + '/arquivoGerado.json', JSON.stringify(produto), err => {
-    console.log(err || 'Arquivo salvo')
-})
+fs.writeFile(
+  __dirname + '/arquivoGerado.json',
+  JSON.stringify(produto),
+  (err) => {
+    console.log(err || 'Arquivo salvo');
+  }
+);
+
+//lendo arquivo direto no require, tem que passar extensao.json
+const leitura = require('./arquivo.json');
+console.log(leitura);
