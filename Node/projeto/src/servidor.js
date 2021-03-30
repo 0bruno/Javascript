21e35a866a3f644cf3c880216554aab8c0bb90f4
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-30 12:17:12 -0300

    feat: import bancoDeDados.js using require

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -3,6 +3,7 @@ const porta = 3003;
 
 const express = require('express');
 const app = express();
+const bancoDeDados = require('./bancoDeDados');
 
 app.get('/produtos', (req, res, next) => {
   res.send({ nome: 'Notebook', preco: 123.45 }); //conveter para json send converte automatico
