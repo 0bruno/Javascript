a9f3275bb36de3a54007f82fb41d139961264d18
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-30 12:27:07 -0300

    feat: using  function getProdutc from bancodeDados using req.params.id

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -7,7 +7,12 @@ const bancoDeDados = require('./bancoDeDados');
 
 app.get('/produtos', (req, res, next) => {
   // res.send({ nome: 'Notebook', preco: 123.45 }); //conveter para json send converte automatico
-  res.send(bancoDeDados.getProduto);
+  res.send(bancoDeDados.getProdutos());
+});
+
+app.get('/produtos/:id', (req, res, next) => {
+  //req.params  == local onde tenho os parametros que coloquei na url
+  res.send(bancoDeDados.getProduto(req.params.id));
 });
 
 app.listen(porta, () => {
