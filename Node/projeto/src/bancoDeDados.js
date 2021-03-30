7137de29f1b6a8f077786b22bd88bcf071c58b36
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-30 11:54:03 -0300

    feat: create function to save product

--- Node/projeto/src/bancoDeDados.js
+++ Node/projeto/src/bancoDeDados.js
@@ -5,3 +5,14 @@ const sequence = {
     return this._id++;
   },
 };
+
+//chave do objeto vai ser cada um do id do produto, e o valor o proprio produto
+const produtos = {};
+
+function salvarProduto(produto) {
+  //se o id nao estiver setado
+  if (!produto.id) produto = sequence.id;
+  produtos[produto.id] = produto;
+  //produto retornado ja possui id
+  return produto;
+}
