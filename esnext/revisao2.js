9350c63ef32a8c34b653295c4d3a9781292728c4
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-31 17:18:20 -0300

    feat: example operator ...rest

--- esnext/revisao2.js
+++ esnext/revisao2.js
@@ -15,3 +15,12 @@ function log(texto = 'node') {
 
 log();
 log('Eu sobrescrevo');
+
+//operador rest
+function total(...numeros) {
+  let total = 0;
+  numeros.forEach((n) => (total += n));
+  return total;
+}
+
+console.log(total(1, 2, 3, 4, 5));
