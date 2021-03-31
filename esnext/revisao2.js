6a66b32d83fa1265b54fc36dba81910da52fcdc7
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-03-31 17:14:15 -0300

    feat: example to this

--- esnext/revisao2.js
+++ esnext/revisao2.js
@@ -1,3 +1,9 @@
 //arrow function
 const soma = (a, b) => a + b;
 console.log(soma(2, 3));
+
+//arrow this
+const lexico1 = () => console.log(this === exports);
+const lexico2 = lexico1.bind({});
+lexico1();
+lexico2();
