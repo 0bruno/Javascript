0b484a191240313d1df524e946f8763783262651
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-01 09:23:46 -0300

    feat: example 2 tagged template

---
+++ esnext/taggedTemplate2.js
@@ -0,0 +1,16 @@
+//funcao que fara um parser em todas as variaveis
+//interpoladas e aplicar uma mascara de dinheiro em cima delas
+
+function real(partes, ...valores) {
+  resultado = [];
+
+  valores.forEach((valor, indice) => {
+    valor = isNaN(valor) ? valor : `R$${valor.toFixed(2)}`;
+    resultado.push(partes[indice], valor);
+  });
+  return resultado.join('');
+}
+
+const preco = 29.9;
+const precoParcela = 11;
+console.log(real`1x de ${preco} ou 3x de ${precoParcela}`);
