9cf74aec5d84bef1858636fc3fb89e4cdc3e369f
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-05 09:31:49 -0300

    feat: example forOf

---
+++ esnext/forOf.js
@@ -0,0 +1,8 @@
+for (let letra of 'Cod3r') {
+  console.log(letra);
+}
+// for of percorre em cima de valores
+const assuntoEcma = ['map', 'set', 'promises'];
+for (let i of assuntoEcma) {
+  console.log(i);
+}
