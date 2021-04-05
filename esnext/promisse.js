bf775925080892bd5417a7240bb8e84507d740c3
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-05 11:41:38 -0300

    feat: example promisse

--- esnext/promisse.js
+++ esnext/promisse.js
@@ -11,3 +11,14 @@ falarDepois(3, 'Estou falando')
     console.log(frase);
   })
   .catch((e) => console.log('Erro'));
+
+//criando depois executando primeiro
+function teste(teste) {
+  return new Promise((resolve) => {
+    resolve(teste);
+  });
+}
+
+teste('oi Bruno').then((teste) => {
+  console.log(teste.replace('oi', 'OI'));
+});
