02868990c69738a50a3724e5cea583e31b0d67b6
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-12 20:08:55 -0300

    feat: more about promisse

--- esnext/primisse_1.js
+++ esnext/primisse_1.js
@@ -1,1 +1,7 @@
 //promisse algo que vai resolver no futuro
+let p = new Promise(function (cumprirPromessa) {
+  cumprirPromessa(['Ana', 'Bia', 'Caio']);
+});
+
+p.then((valor) => valor[0]).then((mostrarLetra) => console.log(mostrarLetra));
+//resultado do metodo Then() e passado pro proximo metodo e assim opr diante
