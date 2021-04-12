6e2fabaee9c067219264e6904e79daa67886aee1
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-12 20:11:37 -0300

    feat: add new code line

--- esnext/primisse_1.js
+++ esnext/primisse_1.js
@@ -3,5 +3,8 @@ let p = new Promise(function (cumprirPromessa) {
   cumprirPromessa(['Ana', 'Bia', 'Caio']);
 });
 
-p.then((valor) => valor[0]).then((mostrarLetra) => console.log(mostrarLetra));
+p.then((valor) => valor[0])
+  .then((primeiro) => primeiro[0])
+  .then((letra) => letra.toLowerCase())
+  .then((mostrar) => console.log(mostrar));
 //resultado do metodo Then() e passado pro proximo metodo e assim opr diante
