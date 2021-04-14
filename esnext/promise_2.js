df1615faefcca2438e6d669012f23218aa1a4d21
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-13 21:49:37 -0300

    feat: executing promise

--- esnext/promise_2.js
+++ esnext/promise_2.js
@@ -2,7 +2,11 @@ function esperaPor(tempo = 2000) {
   return new Promise(function (resolve) {
     setTimeout(function () {
       console.log('executando');
-      resolve();
+      resolve('Executou');
     }, tempo);
   });
 }
+
+esperaPor(5000).then((texto) => {
+  console.log(texto[0]);
+});
