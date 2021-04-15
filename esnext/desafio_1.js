f89b6eb50ad9dea1b28b88a2472f296f830bac9d
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-15 10:51:12 -0300

    feat: promise fs.readFile

--- esnext/desafio_1.js
+++ esnext/desafio_1.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function lerArquivo(caminho) {
-  return new promises((resolve) => {
+  return new Promise((resolve) => {
     fs.readFile(caminho, function (_, conteudo) {
       resolve(conteudo.toString());
     });
@@ -10,3 +10,5 @@ function lerArquivo(caminho) {
 }
 
 const caminho = path.join(__dirname, 'dados.txt');
+
+lerArquivo(caminho).then((conteudo) => console.log(conteudo));
