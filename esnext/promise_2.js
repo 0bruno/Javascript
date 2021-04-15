8d922e2f3d1d9010a62da4b88ecf67e6a0ab9a11
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-14 23:56:39 -0300

    refactor: add ne commentary line

--- esnext/promise_2.js
+++ esnext/promise_2.js
@@ -3,6 +3,7 @@ function esperaPor(tempo = 2000) {
   return new Promise(function (resolve) {
     setTimeout(function () {
       console.log('executando');
+      //chamando resolve
       resolve('Executou');
     }, tempo);
   });
