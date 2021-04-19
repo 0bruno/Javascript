4baa146967c27ba3632a6a1d5f8534d2f1cd432c
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-19 20:49:08 -0300

    feat: example function Then()

---
+++ esnext/asynAwait_1.js
@@ -0,0 +1,10 @@
+function retornaValor() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(10);
+    }, 5000);
+  });
+}
+
+//com then()
+retornaValor().then((valor) => console.log(valor));
