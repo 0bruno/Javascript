d56001da5904ed9d7a385b6ed5f924a724a5a95f
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-19 21:59:20 -0300

    feat: example resolve reject to promise

--- esnext/ayncAwait_2.js
+++ esnext/ayncAwait_2.js
@@ -1,11 +1,19 @@
-function geraNumerosEntre(min, max) {
+function geraNumerosEntre(min, max, numeroRepetido) {
   if (min > max) {
     [max, min] = [min, max];
   }
 
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const fator = max - min + 1;
     const aleatorio = parseInt(Math.random() * fator) + min;
-    resolve(aleatorio);
+    if (numeroRepetido.includes(aleatorio)) {
+      reject('Numero Repetido');
+    } else {
+      resolve(aleatorio);
+    }
   });
 }
+
+geraNumerosEntre(1, 5, [1, 2, 4])
+  .then(console.log)
+  .catch((err) => console.log(err));
