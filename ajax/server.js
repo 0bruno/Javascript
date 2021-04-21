953a46a01ee5a39525ebecdf82826fe833be1512
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-21 18:15:47 -0300

    refactor: add new line commentary for express.static()

--- ajax/server.js
+++ ajax/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 //definindo middlewares, funcoao executada quando determinada requisição chegar
-app.use(express.static('.')); //provendo os arquivos etaticos a partir desse middlware
+app.use(express.static('.')); //provendo todos os arquivos etaticos pra essa pasta atual a partir desse middlware
 app.use(express.urlencoded({ extended: true })); // se vier algum formato a partir de um submit de um form esse codigo le e os transforma em objeto
 app.use(express.json()); //se vier json na requisição este codigo o transforma em objeto
 
