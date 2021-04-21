77f382baab01bd1d69341ccb5f3ea59d869a1d78
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-21 17:02:05 -0300

    feat: create requires

--- ajax/server.js
+++ ajax/server.js
@@ -1,1 +1,4 @@
 // servidor pra prover os dados estaticos
+const bodyParser = require('body-parser'); // deixa os ddos dentro do request pronto para ser manupulados dentro desse arquivo
+const express = require('express');
+const app = express();
