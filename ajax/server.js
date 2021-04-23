d5e437d541c0bc4e76dd9d6f9ca92e8a5702f3fe
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-04-22 23:38:16 -0300

    refactor: add new  commentary line

--- ajax/server.js
+++ ajax/server.js
@@ -8,5 +8,6 @@ app.use(express.static('.')); //provendo todos os arquivos etaticos pra essa pas
 app.use(express.urlencoded({ extended: true })); // se vier algum formato a partir de um submit de um form esse codigo le e os transforma em objeto
 app.use(express.json()); //se vier json na requisição este codigo o transforma em objeto
 
+//criando rota teste
 app.get('/teste', (req, res) => res.send('Ok'));
 app.listen(3000, () => console.log('Excutando'));
