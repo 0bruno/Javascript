7f32938c05898ef5686b27826a3d0f72e0babfb2
Bruno <brunobsribeiro@gmail.com>
2021-06-09 20:55:04 -0300

    feat: create code fizzbuzz to JS

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,1 +1,11 @@
 /*Divisivel por 5 por 3 e por 3 e 5 */
+
+function fizzbuzz(entrada) {
+  if (typeof entrada !== 'number') return 'NOT A NUMBER';
+  if (entrada % 3 == 0 && entrada % 5 == 0) return 'FIZZBUZZ';
+  if (entrada % 3 == 0) return 'FIZZBUZZ';
+  if (entrada % 5 == 0) return 'FIZBUZZ';
+  return entrada;
+}
+
+console.log(fizzbuzz(11));
