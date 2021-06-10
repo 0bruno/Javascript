cdc2a183358e249d535f449a684b68acfb478766
Bruno <brunobsribeiro@gmail.com>
2021-06-09 21:25:04 -0300

    feat: create a Function to calcule Max speed

--- Exercicios/RadarVelocidade.js
+++ Exercicios/RadarVelocidade.js
@@ -3,3 +3,25 @@
  * Math.floor() arredonda para o decimal mais perto
  * Se pontos > 12 enta ocarteira suspensa
  */
+
+function velocidadeMax(velocidade) {
+  const velocidadePermitida = 70;
+  const punicaoPonto = 5;
+  const limitePontos = 12;
+  //recebendo pontos de acordo com velocidade acima do permitido
+  const pontos = Math.floor((velocidade - velocidadePermitida) / punicaoPonto);
+
+  if (velocidade <= velocidadePermitida) {
+    console.log(`${velocidade} é uma velocidade permitida`);
+  } else if (pontos >= limitePontos) {
+    console.log(
+      `Você teve a carteira suspensa por estar a ${velocidade} KM/h e atingir ${pontos} pontos do limite de ${limitePontos} pontos`
+    );
+  } else {
+    console.log(
+      `Voce esta a ${velocidade} KM/h e recebeu ${pontos} pontos pela infração`
+    );
+  }
+}
+
+velocidadeMax(80);
