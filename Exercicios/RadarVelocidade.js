bb0b6fa803617170b41656c8db62722be229d390
Bruno <brunobsribeiro@gmail.com>
2021-06-09 21:08:05 -0300

    refactor: add instruction to create exercice Radar

---
+++ Exercicios/RadarVelocidade.js
@@ -0,0 +1,5 @@
+/**Velocidade máxima de até 70 KM
+ * a cada 5KM acima do limite voce recebe 1 ponto
+ * Math.floor() arredonda para o decimal mais perto
+ * Se pontos > 12 enta ocarteira suspensa
+ */
